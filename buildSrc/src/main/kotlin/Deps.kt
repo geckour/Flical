cd747b1c4e07d9ed1c302cb95c3b1331d1f17b49
geckour <geckour@gmail.com>
2020-07-23 13:06:05 +0900

    Bump dependencies version

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,20 +1,20 @@
 object Deps {
     object Kotlin {
-        const val version = "1.3.50"
-        val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val version = "1.3.72"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
 
         object Coroutines {
-            const val version = "1.3.2"
-            val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
-            val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+            private const val version = "1.3.8"
+            const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+            const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
         }
     }
 
     object GradlePlugin {
-        const val android = "com.android.tools.build:gradle:3.5.2"
+        const val android = "com.android.tools.build:gradle:4.0.1"
         const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
         const val googleService = "com.google.gms:google-services:4.3.3"
-        const val fabric = "io.fabric.tools:gradle:1.31.0"
+        const val fabric = "io.fabric.tools:gradle:1.+"
 
         const val compileSdkVersion = 29
         const val minSdkVersion = 24
@@ -22,20 +22,20 @@ object Deps {
     }
 
     object Test {
-        const val junit = "junit:junit:4.12"
-        const val testRunner = "androidx.test:runner:1.1.2-alpha01"
+        const val junit = "junit:junit:4.13"
+        const val testRunner = "androidx.test:runner:1.2.0"
         const val instrumentTestRunner = "androidx.test.runner.AndroidJUnitRunner"
-        const val espressoCore = "androidx.test.espresso:espresso-core:3.1.2-alpha01"
+        const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
     }
 
     object AndroidX {
-        const val appCompat = "androidx.appcompat:appcompat:1.1.0-alpha02"
-        const val coreKtx = "androidx.core:core-ktx:1.1.0-alpha04"
-        const val design = "com.google.android.material:material:1.1.0-alpha03"
-        const val constraint = "androidx.constraintlayout:constraintlayout:2.0.0-alpha3"
+        const val appCompat = "androidx.appcompat:appcompat:1.2.0-rc01"
+        const val coreKtx = "androidx.core:core-ktx:1.3.0"
+        const val design = "com.google.android.material:material:1.2.0-rc01"
+        const val constraint = "androidx.constraintlayout:constraintlayout:2.0.0-beta8"
 
         object Lifecycle {
-            const val version = "2.1.0-alpha02"
+            private const val version = "2.2.0"
             const val extensions = "androidx.lifecycle:lifecycle-extensions:$version"
             const val viewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
             const val compiler = "androidx.lifecycle:lifecycle-compiler:$version"
@@ -47,21 +47,21 @@ object Deps {
     }
 
     object PermissionDispatcher {
-        const val version = "3.3.1"
+        private const val version = "3.3.1"
         const val permissionDispatcher = "com.github.hotchemi:permissionsdispatcher:$version"
         const val processor = "com.github.hotchemi:permissionsdispatcher-processor:$version"
     }
 
     object BigDecimalMath {
-        const val bigDecimalMath = "ch.obermuhlner:big-math:2.0.1"
+        const val bigDecimalMath = "ch.obermuhlner:big-math:2.3.0"
     }
 
     object Exif {
-        const val exifInterface = "androidx.exifinterface:exifinterface:1.0.0"
+        const val exifInterface = "androidx.exifinterface:exifinterface:1.2.0"
     }
 
     object Firebase {
-        const val core = "com.google.firebase:firebase-core:16.0.4"
-        const val crashlytics = "com.crashlytics.sdk.android:crashlytics:2.9.3"
+        const val core = "com.google.firebase:firebase-core:17.4.4"
+        const val crashlytics = "com.crashlytics.sdk.android:crashlytics:2.10.1"
     }
 }
